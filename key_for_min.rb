d93ae67ce32370d32704087e26a1e07166b79083
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-29 01:17:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
     new_hash = {}
     binding.pry
     name_hash.each do |k, v|
+    binding.pry
     if name_hash[v] < min_value
       name_hash[min_value] = name_hash[v]
       new_hash[0][0] = name_hash[k][min_value]
