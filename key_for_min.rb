d11815c039e6b35b1a8fe90e290de41427d4bab6
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-29 00:36:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_value = 10000
+    new_hash = {}
+    name_hash.each do |k, v|
+    if name_hash[v] < min_value
+      name_hash[k][min_value] = name_hash[k][v]
+      new_hash[0][0] = name_hash[k][min_value]
+    end
+      new_hash[k]
+  end
 end
