43a0501f5b4b1b8b3e215403d6f4215c61eddb6f
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-29 01:23:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,12 @@ require "pry"
 
 def key_for_min_value(name_hash)
     min_value = Float::INFINITY
+    min_key = nil
     new_hash = {}
     name_hash.each do |k, v|
     binding.pry
-    if name_hash[v] < min_value
-      name_hash[min_value] = name_hash[v]
+    if v < min_value
+      min_value = v
       new_hash[0][0] = name_hash[k][min_value]
     end
       new_hash[k]
